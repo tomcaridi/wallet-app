b8a92707060b151885da6d1e9650b1ba5b64d7ba
Tom Caridi <tom.caridi@gmail.com>
2015-11-14 18:01:38 -0500

    form fields styling

--- app/controllers/card_users_controller.rb
+++ app/controllers/card_users_controller.rb
@@ -9,9 +9,10 @@ class CardUsersController < ApplicationController
   	@card = Card.find(params[:id])
   	if CardUser.create(user_id: @user.id, card_id: @card.id)
   	  flash[:notice] = "Card shared"
+      redirect_to user_path(session[:user_id])
   	else 
   	  flash[:alert] = "There was an issue"
+      redirect_to share_path(card.id)
   	end
-    redirect_to user_path(session[:user_id])
   end
 end
